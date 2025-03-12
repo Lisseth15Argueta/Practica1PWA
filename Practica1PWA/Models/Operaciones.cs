1693173e673fe452a203770d2a0ab4944100eb06
Lisseth <arguetalisseth029@gmail.com>
2025-03-11 23:28:50 -0600

    Practica1_DPWA, Suma,Resta,Multiplicacion y division

---
+++ Practica1PWA/Models/Operaciones.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Practica1PWA.Models
+{
+    public class Operaciones
+    {
+        [Required]
+        [Display(Name = "Primer dato")]
+        public double PrimerDato { get; set; } = 0;
+
+        [Required]
+        [Display(Name = "Segundo dato")]
+        public double SegundoDato { get; set; } = 0;
+
+        public double Resultado { get; set; }
+        public int TipoOperacionId { get; set; } = 0;
+        public List<TiposOperacion> ListaOperaciones { get; set; }
+
+        public Operaciones()
+        {
+            ListaOperaciones = new List<TiposOperacion>
+            {
+                new TiposOperacion { Id = 1, Nombre = "Suma" },
+                new TiposOperacion { Id = 2, Nombre = "Resta" },
+                new TiposOperacion { Id = 3, Nombre = "Multiplicación" },
+                new TiposOperacion { Id = 4, Nombre = "División" },
+            };
+        }
+    }
+}
---
+++ Practica1PWA/Models/TiposOperacion.cs
@@ -0,0 +1,8 @@
+﻿namespace Practica1PWA.Models
+{
+    public class TiposOperacion
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
